0b27b8762c69dcd395188be51abdd214453c88f9
VictorRodriguesDosReis <victor_reis005@hotmail.com>
2019-10-04 13:51:04 -0300

    Adicionado a conexão do MongoDB

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
-const server =  express();
+const server =  express();  // Instancia o servidor
 
-server.use(express.json());
-server.use(routes);
+// Conecta com o banco no MongoBD Atlas
+mongoose.connect('mongodb+srv://tindev:tindev@cluster0-k7eex.mongodb.net/tindev?retryWrites=true&w=majority', {
+    useNewUrlParser: true,      // Habilita a utilização do novo "URL string parser"
+    useUnifiedTopology: true,   // Habilita a utilização do novo "Server Discover" e "Monitoring engine"
+});
 
-server.listen(3333);
+server.use(express.json()); // Habilita compreensão de conteúdos tipo JSON
+server.use(routes);         // Utiliza as rotas cridas
+
+server.listen(3333);        // Escutar as requisições na porta 3333
