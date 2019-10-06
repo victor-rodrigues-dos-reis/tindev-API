d05730eee93cc1946eff317ff525945ca61cb21f
VictorRodriguesDosReis <victor_reis005@hotmail.com>
2019-10-06 00:39:30 -0300

    Criado as rotas da API
    Foram criados as rotas de:
    - Cadastro de desenvolvedor
    - Listagem de desenvolvedores
    - Like em desenvolvedor
    - Dislike em desenvolvedor

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
 const express = require('express')
 
+const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.send('Hello World')
-});
+/*--- ROTAS DA API ---*/
+routes.get('/devs', DevController.index)
 
-routes.post('/devs', (request, response) => {
-    return response.json(request.body)
-});
+routes.post('/devs', DevController.create);
+routes.post('/devs/:devId/likes', LikeController.create);
+routes.post('/devs/:devId/dislikes', DislikeController.create);
 
 module.exports = routes;
